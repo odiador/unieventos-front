136d8ca95e2175fd8df291ea8e3bd7ad1a1b0b47
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-14 13:10:20 -0500

    Add watermark

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={inter.className}  >
-        <div className="fixed -z-50 bg-[#1d2228] pointer-events-none left-0 top-0 w-screen h-screen"></div>
+      <body className={`${inter.className} flex flex-col`}  >
+        <div className="fixed -z-50 bg-[#1d2228] pointer-events-none left-0 top-0 w-screen h-screen" />
         {children}
+        <div className="fixed bottom-0 w-full text-white/10 h-10 justify-center flex items-center text-center">
+          <Link href={"https://github.com/odiador"} className="hover:scale-105 transition-all hover:text-white/100 cursor-pointer h-full px-4 content-center">Made with ❤️ by <strong>Amador</strong></Link>
+        </div>
       </body>
     </html>
   );
