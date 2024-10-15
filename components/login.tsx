da0928006dad9d6705c7522d8cf45f86d761c55b
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-15 14:35:19 -0500

    Change method structure to avoid errors
    React Hooks must be called in a React function component or a custom React Hook function.  react-hooks/rules-of-hooks

--- components/login.tsx
+++ components/login.tsx
@@ -1,15 +1,14 @@
-import { useRouter } from "next/navigation";
-import { useModal } from "./modal";
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 function Login({ usePassword, useCode, back, }: { usePassword: Function, useCode: Function, back: Function }) {
-    const { openModal } = useModal();
+    const goToPassword = () => usePassword();
+    const goToCode = () => useCode();
+    const goBack = () => back();
     return (
         <>
             <h1 className="text-center text-2xl font-semibold">Elige una opción de inicio</h1>
-            <button onClick={(() => usePassword())} type="button" className="">Usa tu contraseña</button>
-            <button onClick={(() => useCode())} type="button" className="button-secondary">Usa un código de seguridad</button>
-            <button onClick={(() => back())} type="button" className="button-terciary">Volver</button>
+            <button onClick={goToPassword} type="button" className="">Usa tu contraseña</button>
+            <button onClick={goToCode} type="button" className="button-secondary">Usa un código de seguridad</button>
+            <button onClick={goBack} type="button" className="button-terciary">Volver</button>
         </>);
 }
 export { Login };
