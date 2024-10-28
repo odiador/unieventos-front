e45b88d84f8844af84d65ff057782a611e805e68
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-28 13:22:43 -0500

    Add a navbar and the responsive impl

--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,56 @@
 "use client";
 import { useAuthContext } from "@/api/utils/auth";
+import { IconBrandAuth0, IconBrandOauth, IconDots, IconLogin, IconLogout2, IconRegistered, IconUserPlus } from "@tabler/icons-react";
 import Link from "next/link";
 import { useEffect } from "react";
 
 const Header = () => {
+    const headerValues = [
+        {
+            id: "events",
+            name: "Eventos",
+            href: "/events"
+        },
+        {
+            id: "cals",
+            name: "Calendarios",
+            href: "/calendars"
+        },
+    ]
     const { account, signout } = useAuthContext();
     const { updateRole } = useAuthContext();
     useEffect(() => { updateRole() }, [])
     return (
-        <header className="sticky top-0 bg-black/10 z-50 backdrop-blur-lg flex flex-col justify-center items-stretch sm:flex-row w-full px-4 py-2 gap-2">
-            <div className="grow">
-                <Link href={"/"} className="px-2 text-2xl font-extrabold text-white/70 select-none">{"amaTickets"}</Link>
+        <header className="sticky top-0 bg-black/10 z-50 backdrop-blur-lg flex justify-center items-stretch w-full px-4 py-2 gap-2">
+            <div className="grow h-full flex items-center gap-1 sm:justify-normal justify-between">
+                <Link href={"/"} className="px-3 text-2xl font-extrabold text-white/70 hover:text-white transition-colors select-none">{"amaTickets"}</Link>
+                <div className="hidden gap-[2px] shrink sm:flex items-center">
+                    {account && account.role === "ADMINISTRATOR" && <Link className="px-2 relative text-lg transition-colors font-semibold text-white/50 hover:text-white" href={"/dashboard"}>Dashboard</Link>}
+                    {headerValues.map(values => {
+                        return (
+                            <>
+                                {<label>|</label>}
+                                <Link
+                                    key={values.id}
+                                    className="px-2 relative text-lg transition-colors font-semibold text-white/50 hover:text-white"
+                                    href={values.href}>
+                                    {values.name}
+                                </Link>
+                            </>)
+                    })}
+                </div>
+                <div className="block sm:hidden">
+                    <button className="button-icon" onClick={() => { }}>
+                        <IconDots />
+                    </button>
+                </div>
             </div>
-            {account && <button onClick={() => signout()}>Cerrar Sesión</button>}
-            {!account && <Link href={"/auth/login"} className="button">Inicia Sesión</Link>}
-            {!account && <Link href={"/auth/signup"} className="button button-secondary">Regístrate</Link>}
+            {account && <button className="hidden sm:block" onClick={() => signout()}>Cerrar Sesión</button>}
+            {account && <button className="block sm:hidden button-icon" onClick={() => signout()}><IconLogout2 /></button>}
+            {!account && <Link href={"/auth/login"} className="button sm:block hidden">Inicia Sesión</Link>}
+            {!account && <Link href={"/auth/login"} className="button block sm:hidden button-icon"><IconLogin /></Link>}
+            {!account && <Link href={"/auth/signup"} className=" sm:block hidden button button-secondary">Regístrate</Link>}
+            {!account && <Link href={"/auth/signup"} className="button button-secondary button-icon block sm:hidden"><IconUserPlus /></Link>}
         </header>
     );
 }
