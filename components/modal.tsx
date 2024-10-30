429c4b878ef7a8991d0f041f07087c9527bed955
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-30 00:58:09 -0500

    Add custom modal option

--- components/modal.tsx
+++ components/modal.tsx
@@ -5,6 +5,7 @@ interface ModalContextType {
   isOpen: boolean;
   content: string;
   openModal: (content: string) => void;
+  openCustomModal: (content: ReactNode) => void;
   closeModal: () => void;
 }
 
@@ -13,9 +14,14 @@ const ModalContext = createContext<ModalContextType | undefined>(undefined);
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [content, setContent] = useState('');
+  const [customContent, setCustomContent] = useState<ReactNode | undefined>(undefined);
 
   const openModal = (content: string) => {
     setContent(content);
+    setIsOpen(true);
+  };
+  const openCustomModal = (content: ReactNode) => {
+    setCustomContent(content);
     setIsOpen(true);
   };
 
@@ -25,7 +31,7 @@ export const ModalProvider = ({ children }: { children: ReactNode }) => {
   };
 
   return (
-    <ModalContext.Provider value={{ isOpen, content, openModal, closeModal }}>
+    <ModalContext.Provider value={{ isOpen, content, openModal, closeModal, openCustomModal }}>
       {children}
       {isOpen && (
         <motion.div
@@ -33,14 +39,15 @@ export const ModalProvider = ({ children }: { children: ReactNode }) => {
           animate={{ opacity: 1 }}
           transition={{ duration: .1 }}
           className="fixed top-0 left-0 bg-black/50 z-50 w-full h-full flex justify-center items-center px-2 sm:px-8 py-12">
-          <motion.div
+          {customContent}
+          {content && <motion.div
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             transition={{ duration: .1 }}
             className="flex flex-col w-4/5 overflow-hidden h-full max-h-64 items-end gap-4 bg-white rounded-2xl shadow-sm shadow-black/45 outline-black/50">
-            <span className="mt-2 mr-4 cursor-pointer text-black rounded-full text-4xl font-medium size-4 text-center flex items-center justify-center" onClick={closeModal}>&times;</span>
-            <p className='mx-4 mb-2 overflow-hidden text-black self-start h-full text-wrap'>{content}</p>
-          </motion.div>
+            {<span className="mt-2 mr-4 cursor-pointer text-black rounded-full text-4xl font-medium size-4 text-center flex items-center justify-center" onClick={closeModal}>&times;</span>}
+            {<p className='mx-4 mb-2 overflow-hidden text-black self-start h-full text-wrap'>{content}</p>}
+          </motion.div>}
         </motion.div>
       )}
     </ModalContext.Provider>
