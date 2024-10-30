83d7f59cb186f9b07898bc6e4774625850f1a780
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-10-29 21:13:25 -0500

    Add a cropping to image uploading
    Pues eso jajaj que cuando uno suba una imagen se pueda hacer un corte con una relación de aspecto

--- app/dashboard/@admin/calendars/edit/[id]/[eventid]/page.tsx
+++ app/dashboard/@admin/calendars/edit/[id]/[eventid]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { findEvent } from "@/api/utils/api";
 import { FindEventDTO } from "@/api/utils/schemas";
+import DropZone from "@/components/dropzone";
 import { useModal } from "@/components/modal";
+import { IconEdit } from "@tabler/icons-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -9,14 +11,12 @@ const EventPage = ({ params }: { params: { id: string, eventid: string } }) => {
     const [eventFound, setEvent] = useState<FindEventDTO | null>(null);
     const [loading, setLoading] = useState(true);
     const { openModal } = useModal();
-    const [evtImage, setEventImage] = useState<string | null>(null);
+    const [evtImage, setEventImage] = useState<string | undefined>(undefined);
     useEffect(() => {
-        console.log(params);
 
         findEvent({ idCalendar: params.id, name: decodeURIComponent(params.eventid) }).then((response) => {
             if (response.status == 200) {
                 setEvent(response.data.response);
-                setEventImage(response.data.response?.eventImage || null)
                 setLoading(false);
             } else {
                 setEvent(null);
@@ -28,11 +28,19 @@ const EventPage = ({ params }: { params: { id: string, eventid: string } }) => {
     return <>
         {!loading && !eventFound && <label>No fue encontrado</label>}
         {loading && <label>Cargando...</label>}
+
         {!loading && eventFound && (
-            <div className="flex flex-col gap-2 p-8">
-                <input className="border-0 bg-transparent font-bold caret- text-white text-5xl" placeholder="Nombre del Evento" defaultValue={eventFound.name} />
+            <div className="flex flex-col gap-2 p-8 items-center relative">
+                <div className="flex items-center">
+                    <input className="border-0 bg-transparent font-bold text-white text-5xl" placeholder="Nombre del Evento" defaultValue={eventFound.name} />
+                    <IconEdit className="scale-150 cursor-pointer text-[#9ca3af] hover:text-white transition-colors" onClick={() => {
+
+                    }} />
+                </div>
+                <div className="px-8 py-2 bg-white bg-opacity-[.02]">
+                    <DropZone aspect={1} croppedImage={evtImage} setImageCropped={setEventImage} initialImage={eventFound.eventImage} />
+                </div>
                 {evtImage && (<Image src={evtImage} width={1024} height={1024} className="w-40 h-fit" alt={evtImage} />)}
-                <input type="file" />
                 <input type="file" />
             </div>
         )}
--- components/dropzone.tsx
+++ components/dropzone.tsx
@@ -1,64 +1,91 @@
 "use client";
 
+import { getCroppedImg } from "@/api/utils/imagecropping";
 import { IconEdit, IconPlus, IconUpload, IconX } from "@tabler/icons-react";
 import { motion, useAnimation } from "framer-motion";
 import Image from "next/image";
-import { Dispatch, SetStateAction, useCallback, useEffect } from "react";
+import { Dispatch, Fragment, SetStateAction, useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import Cropper, { Area, Point } from "react-easy-crop";
 
-const DropZone = ({ img, setImage }: { img?: string, setImage: Dispatch<SetStateAction<string | undefined>> }) => {
+const DropZone = ({ croppedImage, aspect, setImageCropped, initialImage }: { croppedImage?: string, setImageCropped: Dispatch<SetStateAction<string | undefined>>, initialImage: string, aspect: number }) => {
     const onDrop = useCallback((acceptedFiles: File[]) => {
-        setImage(URL.createObjectURL(acceptedFiles[0]))
+        setImg(URL.createObjectURL(acceptedFiles[0]))
     }, [])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     const control2 = useAnimation();
     const control = useAnimation();
+    const [img, setImg] = useState<string | undefined>();
+    const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
     useEffect(() => {
         control2.start(isDragActive ? "show" : "noshow");
         control.start(isDragActive ? "show" : "noshow");
     }, [isDragActive])
+
+
+    useEffect(() => {
+        if (croppedImage && croppedImage !== process.env.NEXT_PUBLIC_DEFAULT_IMAGE && croppedImage !== initialImage) {
+
+        }
+    }, [croppedImage])
+    useEffect(() => {
+        console.log(initialImage);
+        setImageCropped(initialImage)
+    }, [])
     return (
         <div className="rounded-lg w-fit group flex flex-col"
             onMouseEnter={() => control.start("show")}
             onMouseLeave={() => control.start("noshow")}
         >
+            {img && <div className="top-0 left-0 absolute w-full flex flex-col items-center h-full bg-black/20 z-10">
+                <Fragment>
+                    <button className="z-10" onClick={() => {
+                        if (croppedAreaPixels) {
+                            getCroppedImg(img, croppedAreaPixels, { horizontal: false, vertical: false }).then((img) => {
+                                setImageCropped(img || "")
+                                setImg(undefined);
+                            }).catch(() => { })
+                        }
+                    }}>Cortar</button>
+                    <Cropper classes={{ containerClassName: "-z-10" }}
+                        image={img}
+                        aspect={aspect}
+                        onCropChange={setCrop}
+                        crop={crop}
+                        onCropComplete={(croppedArea, croppedAreaPixels) => { setCroppedAreaPixels(croppedAreaPixels) }}
+                    >
+                    </Cropper>
+                </Fragment>
+            </div>}
             <motion.div
-                variants={{
-                    show: {
-                        y: 0,
-                    },
-                    noshow: {
-                        y: 30
-                    }
-                }}
+                variants={{ show: { y: 0, }, noshow: { y: 30 } }}
                 animate={control}
                 transition={{ duration: 0.2 }}
                 className="text-xs bg-white w-full flex rounded-t-lg text-black justify-end pr-2">
                 <IconX className="cursor-pointer"
                     onClick={() => {
-                        setImage(process.env.NEXT_PUBLIC_DEFAULT_IMAGE)
+                        setImageCropped(process.env.NEXT_PUBLIC_DEFAULT_IMAGE)
                     }} />
 
             </motion.div>
-            <section className="relative size-40 cursor-pointer z-[1] bg-black rounded-b-lg" >
-                <div {...getRootProps()} className="w-full h-full z-10 outline-none flex items-center justify-center">
+            <section className="relative w-40 cursor-pointer z-[1] bg-black rounded-b-lg" >
+                <div {...getRootProps()} className="absolute top-0 left-0 size-full z-10 outline-none flex items-center justify-center">
                     <input {...getInputProps({ multiple: false, accept: "image/*" })} />
                     {isDragActive && <IconUpload />}
-                    {!isDragActive && !img && <IconPlus />}
-                    {!isDragActive && img && <IconEdit />}
+                    {!isDragActive && !croppedImage && <IconPlus />}
+                    {!isDragActive && croppedImage && <IconEdit />}
                 </div>
                 {<motion.div
                     animate={control2}
                     initial="noshow"
-                    variants={{
-                        show: { opacity: 0.25 },
-                        noshow: { opacity: 1 },
-                    }}
+                    variants={{ show: { opacity: 0.25 }, noshow: { opacity: 1 }, }}
                     transition={{ duration: 0.1 }}
+                    className="z-10"
                 >
-                    <Image className="size-full group-hover:opacity-25 rounded-b-lg absolute top-0 opacity-100 transition-opacity left-0 select-none pointer-events-none"
-                        width={512} height={512} src={img || process.env.NEXT_PUBLIC_DEFAULT_IMAGE || ""} alt={img || process.env.NEXT_PUBLIC_DEFAULT_IMAGE || ""} />
+                    <Image className="size-full group-hover:opacity-25 rounded-b-lg opacity-100 transition-opacity select-none pointer-events-none"
+                        width={512} height={512} src={croppedImage || process.env.NEXT_PUBLIC_DEFAULT_IMAGE || ""} alt={img || process.env.NEXT_PUBLIC_DEFAULT_IMAGE || ""} />
                 </motion.div>}
             </section>
 
